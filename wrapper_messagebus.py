c33a7a2f6eada3ae080b79a87b78d0cad66e8e6e
Gijs Wobben <gijs.wobben@arboned.nl>
2018-03-09 10:32:16 +0100

    Added a modified version of the code to run with messagebus

---
+++ wrapper_messagebus.py
@@ -0,0 +1,66 @@
+# Imports
+import json
+import numpy
+import pickle
+
+from nameko.rpc import rpc, RpcProxy
+
+
+class IrisClassifierService(object):
+	""" This class wraps the model and is used by Nameko
+		to start the service.
+	"""
+
+	name = 'iris_classifier_service'
+	model = pickle.load(open('models/iris_classifier.pickle', 'rb'))
+
+
+	def predict(self, data):
+		""" Method that will make the actual prediction.
+		"""
+
+		# Convert the data into the same format as during training (numpy.array)
+		data = numpy.array(data)
+
+		# Use the model to predict the class
+		return self.model.predict(data)
+
+
+	@rpc
+	def classify(self, new_data):
+		""" This method will handle incoming data,
+			run it through the model and return the
+			classified output.
+		"""
+
+		try:
+
+			# Parse the input data (JSON)
+			new_data = json.loads(new_data)
+
+			# Make the prediction
+			prediction = self.predict(new_data)
+
+			# Return the prediction
+			return json.dumps({
+				'status': 'success',
+				'message': 'Classified input data',
+				'predictions': prediction.tolist()
+			})
+
+		except ValueError:
+
+			# Send a message back when the input is not valid JSON
+			return json.dumps({
+				'status': 'failed',
+				'message': 'Input is not valid JSON data'
+			})
+
+		except Exception as e:
+
+			# Send a message back when another error occurs (with the error message)
+			return json.dumps({
+				'status': 'failed',
+				'message': 'Some error occured',
+				'error': e
+			})
